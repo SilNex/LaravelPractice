14606ca88215f4986f5f64514e46b85a934f90c3
silnex <silnex@silnex.kr>
2019-01-07 18:22:02 +0900

    Retry post test

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -5,11 +5,6 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use App\Post;
-use Illuminate\Support\Facades\Auth;
-use App\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Schema;
 
 class PostTest extends TestCase
 {
