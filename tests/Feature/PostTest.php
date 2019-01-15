ee742bc9442c20ed2348ed6a0111d9ffe2dd7dcd
silnex <silnex@silnex.kr>
2019-01-15 17:31:22 +0900

    Add create, read test with password

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -15,7 +15,6 @@ class PostTest extends TestCase
     /** @test */
     function create_post_without_password()
     {
-
         $user = factory(User::class)->create();
         $post = factory(Post::class)->make();
         
@@ -26,15 +25,46 @@ class PostTest extends TestCase
     }
 
     /** @test */
+    function create_post_with_password()
+    {
+        $user = factory(User::class)->create();
+        $post = factory(Post::class)->make([
+            'password' => 'MyTestPassword',
+        ]);
+        
+        $response = $this->actingAs($user)->post('/posts', $post->makeVisible('password')->toArray());
+
+        $response->assertRedirect('/posts/1');
+        $this->get('/posts/1')->assertViewIs('posts.passCheck');
+    }
+
+    /** @test */
     function read_post_without_password()
     {
         $user = factory(User::class)->create();
         $post = factory(Post::class)->make();
 
-        $response = $this->actingAs($user)->post('/posts', $post->toArray());
+        $this->actingAs($user)->post('/posts', $post->toArray());
         $response = $this->get('/posts/1');
 
         $response->assertSee($post->title);
         $response->assertSee($post->description);
+    }
+
+    /** @test */
+    function read_post_with_password()
+    {
+        $user = factory(User::class)->create();
+        $post = factory(Post::class)->make([
+            'password' => 'MyTestPassword',
+        ]);
+        
+        $this->actingAs($user)->post('/posts', $post->makeVisible('password')->toArray());
+        $response = $this->post('/posts/1',[
+            'password' => 'MyTestPassword',
+        ]);
+
+        $response->assertRedirect('/posts/1');
+        $response->assertSessionHas('post_1_password');
     }
 }
