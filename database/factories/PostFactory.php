c55fc336f7e1f7c477c69e42582fc8445cbc7d9f
silnex <silnex@silnex.kr>
2019-01-15 17:06:54 +0900

    Add post create, read test

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -7,6 +7,5 @@ $factory->define(App\Post::class, function (Faker $faker) {
         'title' => $faker->sentence(5),
         'description' => implode($faker->paragraphs(5)),
         'password' => ((bool)rand(0, 1) ? 'secret' : null),
-        'user_id' => 1, // post create Test user only 1
     ];
 });
--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -5,8 +5,36 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Post;
+use App\User;
 
 class PostTest extends TestCase
 {
-    // 
+    use RefreshDatabase;
+
+    /** @test */
+    function create_post_without_password()
+    {
+
+        $user = factory(User::class)->create();
+        $post = factory(Post::class)->make();
+        
+        $response = $this->actingAs($user)->post('/posts', $post->toArray());
+
+        $response->assertRedirect('/posts/1');
+        $this->assertDatabaseHas('posts', ['title' => $post->title, 'description' => $post->description]);
+    }
+
+    /** @test */
+    function read_post_without_password()
+    {
+        $user = factory(User::class)->create();
+        $post = factory(Post::class)->make();
+
+        $response = $this->actingAs($user)->post('/posts', $post->toArray());
+        $response = $this->get('/posts/1');
+
+        $response->assertSee($post->title);
+        $response->assertSee($post->description);
+    }
 }
