3628562b59b95805b2a66b949bf4cecedfb38b81
silnex <silnex@silnex.kr>
2019-02-01 23:34:47 +0900

    Daily commit

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,10 +3,17 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
-
     return [
         'title' => $faker->sentence(5),
         'description' => implode($faker->paragraphs(5)),
         'password' => null,
+    ];
+});
+
+$factory->state(App\Post::class, 'have_password', function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(5),
+        'description' => implode($faker->paragraphs(5)),
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
     ];
 });
