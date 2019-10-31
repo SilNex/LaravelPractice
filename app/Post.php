0dc7b873ec68e3cda83539c2e5aedfe73c66f434
silnex <silnex@silnex.kr>
2019-10-31 17:22:35 +0900

    Add relation

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $guarded = [];
+
+    protected $casts = [];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'writer_id');
+    }
 }
