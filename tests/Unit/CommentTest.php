a532fead3cf1f98d4c4ac6252fec45e6576971f4
silnex <silnex@silnex.kr>
2019-10-31 17:33:51 +0900

    Create Commnet test

---
+++ tests/Unit/CommentTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CommentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory('App\User')->create();
+        $this->board = factory('App\Board')->create();
+        $this->post = factory('App\Post')->create([
+            'board_id' => $this->board->id,
+            'user_id' => $this->user->id,
+        ]);
+    }
+
+    public function testCreateComment(): void
+    {
+        $comment = factory('App\Comment')->create([
+            'post_id' => $this->post->id,
+            'user_id' => $this->user->id,
+        ]);
+
+        $this->assertDatabaseHas('comments', $comment->toArray());
+    }
+}
