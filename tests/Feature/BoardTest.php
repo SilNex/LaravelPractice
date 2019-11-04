c20928a22dd279226b10dbaf63d687d1a4baaaf7
silnex <silnex@silnex.kr>
2019-11-05 01:18:21 +0900

    Made board create test

--- tests/Feature/BoardTest.php
+++ tests/Feature/BoardTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class BoardTest extends TestCase
@@ -13,28 +12,23 @@ class BoardTest extends TestCase
 
     protected function setUp(): void
     {
-        $user = factory('App\User')->create();
-        Auth::login($user);
-        $this->user = auth();
+        parent::setUp();
+        $this->user = factory('App\User')->create();
         $this->board = factory('App\Board')->create();
     }
 
     public function testCreateBoard(): void
     {
-        $this->actingAs($this->user)->post(route('board.create', [], false), [
+        $this->actingAs($this->user)->post('/board', [
             'name' => 'free',
             'display_name' => '자유게시판',
-        ])->assertCreated()
-            ->assertRedirect('/board/free');
+        ])->assertCreated()->assertRedirect('/board/free');
     }
 
     public function testToLongBoardName(): void
     {
-        $this->followingRedirects()
-            ->actingAs($this->user)
-            ->post(route('board.create', [], false), [
-                'name' => str_repeat('A',256),
-                'display_name' => 'A',
+        $this->post('/board', [
+                'name' => str_repeat('A',256)
             ])->assertSessionHasErrors(['name']);
     }
 }
