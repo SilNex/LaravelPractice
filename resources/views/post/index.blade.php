1b994960692224d96ddd7b8f108643f164890e0c
silnex <silnex@silnex.kr>
2019-11-28 18:03:38 +0900

    Create post index blade

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">post list</div>
+                <div class="card-body">
+                    <div class="list-group list-group-flush">
+                        @foreach ($posts as $post)
+                        <a href="{{route('post.show', [$board, $post->id])}}" class="list-group-item list-group-item-action">{{ $post->title }}</a>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="text-right mt-2">
+                    <a class="btn btn-primary" href="{{route('post.create', $board)}}" role="button">Create Post</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
