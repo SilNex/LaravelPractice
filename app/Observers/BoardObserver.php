369050ecd1b009a52d57fa5692960230c2b98520
silnex <silnex@silnex.kr>
2019-12-03 19:19:27 +0900

    Create board observer

---
+++ app/Observers/BoardObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\Board;
+
+class BoardObserver
+{
+    /**
+     * Handle the board "created" event.
+     *
+     * @param  \App\Board  $board
+     * @return void
+     */
+    public function created(Board $board)
+    {
+        //
+    }
+
+    /**
+     * Handle the board "updated" event.
+     *
+     * @param  \App\Board  $board
+     * @return void
+     */
+    public function updated(Board $board)
+    {
+        //
+    }
+
+    /**
+     * Handle the board "deleted" event.
+     *
+     * @param  \App\Board  $board
+     * @return void
+     */
+    public function deleted(Board $board)
+    {
+        //
+    }
+
+    /**
+     * Handle the board "restored" event.
+     *
+     * @param  \App\Board  $board
+     * @return void
+     */
+    public function restored(Board $board)
+    {
+        //
+    }
+
+    /**
+     * Handle the board "force deleted" event.
+     *
+     * @param  \App\Board  $board
+     * @return void
+     */
+    public function forceDeleted(Board $board)
+    {
+        //
+    }
+}
