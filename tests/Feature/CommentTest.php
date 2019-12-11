55f91106a0c3989068085b38886b19226f7fcb25
silnex <silnex@silnex.kr>
2019-12-11 13:15:47 +0900

    Add comment test

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -3,20 +3,34 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Spatie\Permission\Models\Permission;
 use Tests\TestCase;
 
 class CommentTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory('App\User')->create();
+        $this->board = factory('App\Board')->create();
+        $permission = Permission::create(['name' => "{$this->board->name} use"]);
+        $this->user->givePermissionTo($permission);
+
+        $this->post = factory('App\Post')->create([
+            'board_id' => $this->board->id,
+            'user_id' => $this->user->id,
+        ]);
+        $this->comment = factory('App\Comment')->create([
+            'post_id' => $this->post->id,
+            'user_id' => $this->user->id,
+        ]);
+    }
+
+    public function testGetCommentList(): void
+    {
+        $this->actingAs($this->user)->get("/{$this->board->name}/posts/{$this->post->id}")
+            ->assertViewHas('comments', $this->post->comments);
     }
 }
