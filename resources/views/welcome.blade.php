9ba6c21029c464acc4d329df6a0b06956f39b2f3
silnex <silnex@silnex.kr>
2019-12-30 22:53:01 +0900

    Bootstrap vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,48 +28,33 @@
 
 <body>
     <div id='app'>
-        <header class="text-center">
+        <header class="text-center mt-5">
             <h1>
                 Logo Here
             </h1>
         </header>
 
-        <b-container fluid class="d-flex min-vh-75">
-            <nav class="col-md-3 col-xl-2">
-                <ul>
-                    <li><a href="#" class="text-decoration-none">Link1</a></li>
-                    <li><a href="#" class="text-decoration-none">Link2</a></li>
-                    <li><a href="#" class="text-decoration-none">Link3</a></li>
-                </ul>
-            </nav>
-            <div class="col-md-9 col-xl-8 py-md-3 pl-md-5">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
-            </div>
-            <aside class="col-md-3 col-xl-2">
-                <ul>
-                    <li>subLink1</li>
-                    <li>subLink2</li>
-                    <li>subLink3</li>
-                </ul>
-            </aside>
+        <b-container class="d-flex min-vh-75">
+            <b-row>
+                <b-col md="2">
+                    <b-list-group>
+                        <b-list-group-item v-for="link in [1,2,3,4]">
+                            Link @{{ link }}
+                        </b-list-group-item>
+                    </b-list-group>
+                </b-col>
+                <b-col md="8">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae ipsam blanditiis reiciendis repellat consequatur pariatur. Quae fuga ipsa sapiente magni ipsam. Distinctio ab dicta, corrupti odio in eligendi nesciunt.</p>
+                </b-col>
+                <b-col md="2">
+                    <b-list-group>
+                        <b-list-group-item href="#some-link">Awesome link</b-list-group-item>
+                        <b-list-group-item href="#" active>Link with active state</b-list-group-item>
+                        <b-list-group-item href="#">Action links are easy</b-list-group-item>
+                        <b-list-group-item href="#foobar" disabled>Disabled link</b-list-group-item>
+                    </b-list-group>
+                <b-col>
+            </b-row>
         </b-container>
 
         <footer>
