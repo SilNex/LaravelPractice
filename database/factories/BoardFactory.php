b96123c840b2899878e0effb3d01bc4faf545d55
silnex <silnex@silnex.kr>
2020-01-01 23:20:39 +0900

    Unique board name

--- database/factories/BoardFactory.php
+++ database/factories/BoardFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Board::class, function (Faker $faker) {
     return [
-        'name' => $faker->word,
+        'name' => $faker->unique()->word,
         'explain' => implode(PHP_EOL, $faker->sentences(3)),
     ];
 });
