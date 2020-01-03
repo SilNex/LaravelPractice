cb970d175e95efabdb0fc9920c7d322c1f8f09f4
silnex <silnex@silnex.kr>
2020-01-03 18:00:32 +0900

    Edit Seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,11 +1,15 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UsersTableSeeder extends Seeder
 {
     public function run()
     {
         factory('App\User', 5)->create();
+        $admin = factory('App\User')->states('silnex')->create();
+        $role = Role::create(['name' => 'Admin']);
+        $admin->assignRole($role);
     }
 }
